93a89d3d83a802e32f1406ff9c4f981c3e1dc524
sidsinghrajput <siddharthsingh2014@gmail.com>
2021-12-06 02:43:12 +0530

    completed the 12 key piano

--- src/components/Key.js
+++ src/components/Key.js
@@ -1,19 +1,30 @@
+import _ from 'lodash';
 import React from 'react';
 
 import './Key.css'
+import { NOTE_TO_KEY } from '../global/constants';
 
 class Key extends React.Component {
+    
     noteIsFlat = (note) => {
         return note.length > 1;
+    }
+    
+    keyIsPressed = (note, pressedKeys) => {
+        return _.includes(pressedKeys, NOTE_TO_KEY[note]);
     }
 
 render() {
         let keyClassName = "key";
 
         const noteIsFlat = this.noteIsFlat(this.props.note);
+        const keyIsPressed = this.keyIsPressed(this.props.note, this.props.pressedKeys);
     
         if(noteIsFlat) {
             keyClassName += " flat";
+        }
+        if (keyIsPressed) {
+            keyClassName += " pressed";
         }
     
         let key;
--- src/global/constants.js
+++ src/global/constants.js
@@ -1,6 +1,40 @@
+const VALID_BLACK_KEYS = ['s', 'd', 'g', 'h', 'j'];
+const VALID_WHITE_KEYS = ['z', 'x', 'c', 'v', 'b', 'n', 'm'];
+const VALID_KEYS = [...VALID_BLACK_KEYS, ...VALID_WHITE_KEYS];
+
 const NOTES = [
     'c', 'df', 'd', 'ef',
     'e', 'f', 'gf', 'g',
     'af', 'a', 'bf', 'b'];
 
-export {NOTES};
+const KEY_TO_NOTE = {
+    z: 'c',
+    s: 'df',
+    x: 'd',
+    d: 'ef',
+    c: 'e',
+    v: 'f',
+    g: 'gf',
+    b: 'g',
+    h: 'af',
+    n: 'a',
+    j: 'bf',
+    m: 'b',
+};
+
+const NOTE_TO_KEY = {
+    c  : 'z',
+    df : 's',
+    d  : 'x',
+    ef : 'd',
+    e  : 'c',
+    f  : 'v',
+    gf : 'g',
+    g  : 'b',
+    af : 'h',
+    a  : 'n',
+    bf : 'j',
+    b  : 'm',
+};
+
+export {NOTES, NOTE_TO_KEY, KEY_TO_NOTE, VALID_KEYS};
